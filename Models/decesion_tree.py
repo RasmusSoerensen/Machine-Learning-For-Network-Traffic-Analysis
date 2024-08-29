c963cbfc0b0c8765a4b6c10fc94b931c3d8e2a42
Rasmus2001 <s214622@student.dtu.dk>
2024-08-29 15:04:39 +0200

    Basic models

--- Models/decesion_tree.py
+++ Models/decesion_tree.py
@@ -1,9 +1,10 @@
+import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score
 
-#importing the processed data
+#Import data
 data = pd.read_csv("Data\\processed_data.csv")
 
 #Splitting the data into features and target
@@ -13,17 +14,15 @@ y = data["label"]
 #Splitting the data into training and testing data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-#Creating the Random Forest Classifier
-rf = RandomForestClassifier(n_estimators=100, random_state=42)
-rf.fit(X_train, y_train)
+#Creating the Decision Tree Classifier
+dt = DecisionTreeClassifier(random_state=42)
+dt.fit(X_train, y_train)
 
 #Predicting the target attribute
-y_pred = rf.predict(X_test)
+y_pred = dt.predict(X_test)
 
 #Evaluating the model
 print(classification_report(y_test, y_pred))
 
 #print the accuracy score
 print("Accuracy: ", accuracy_score(y_test, y_pred))
-
-
